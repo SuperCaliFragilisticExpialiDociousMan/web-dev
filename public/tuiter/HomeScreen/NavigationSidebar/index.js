a7bb4f3ada083cc41e5cee2a63ec4ffe7e9f66c9
SuperCaliFragilisticExpialiDociousMan <91343063+SuperCaliFragilisticExpialiDociousMan@users.noreply.github.com>
2022-03-01 00:48:10 -0500

    a5

--- public/tuiter/HomeScreen/NavigationSidebar/index.js
+++ public/tuiter/HomeScreen/NavigationSidebar/index.js
@@ -1,97 +1,67 @@
 const NavigationSidebar = () => {
-
     return (`
-<div class="list-group">
+<div class="list-group" style="background-color: #1e2125;">
 
-<a class="list-group-item disabled" href=" "> 
+<a class="list-group-item disabled" href=" " style="background-color: #1e2125;"> 
 <i class="fab fa-twitter" style="color: white"></i>
 </a >
 
-<!-- continue the rest of the list --> 
-<a class="list-group-item " href="../../HomeScreen/ExploreScreen/ExploreScreen.html" id="ExploreScreen">
+<a class="list-group-item " href="../../HomeScreen/ExploreScreen/ExploreScreen.html" id="ExploreScreen" style="background-color: #1e2125; color: white;">
 <i class="fa fa-home"></i>
 <div class="d-none d-xxl-inline d-xl-inline">Home</div>
 </a > 
 
-<a class="list-group-item " href= "../../ExploreScreen/ExploreScreen/explore.html" id="explore">
+<a class="list-group-item " href= "../../ExploreScreen/ExploreScreen/explore.html" id="explore" style="background-color: #1e2125; color: white;">
 <i class="fa fa-hashtag"></i>
 <div class="d-none d-xxl-inline d-xl-inline">Explore</div>
 </a > 
 
-<a class="list-group-item " href="#" id = "notifications"> 
+<a class="list-group-item " href="#" id = "notifications" style="background-color: #1e2125; color: white;"> 
 <i class="fa fa-bell"></i>
 <div class="d-none d-xxl-inline d-xl-inline">Notification</div>
 </a > 
 
-<a class="list-group-item " href="#" id = "message">
+<a class="list-group-item " href="#" id = "message" style="background-color: #1e2125; color: white;">
 <i class="fa fa-envelope"></i>
 <div class="d-none d-xxl-inline d-xl-inline">Message</div>
 </a > 
 
-<a class="list-group-item " href="#" id = "bookmarks">
+<a class="list-group-item " href="#" id = "bookmarks" style="background-color: #1e2125; color: white;">
 <i class="fa fa-bookmark"></i>
 <span class="d-none d-xxl-inline d-xl-inline">Bookmarks</span>
 </a > 
 
-<a class="list-group-item" href="#" id = "lists">
+<a class="list-group-item" href="#" id = "lists" style="background-color: #1e2125; color: white;">
 <i class="fa fa-list"></i>
 <span class="d-none d-xxl-inline d-xl-inline">Lists</span>
 </a > 
 
-<a class="list-group-item" href="#" id = "more">
+<a class="list-group-item" href="#" id = "more" style="background-color: #1e2125; color: white;">
 <i class="fa fa-ellipsis-h"></i>
 <span class="d-none d-xxl-inline d-xl-inline">More</span>
 </a > 
 
 </div>
 <div class="d-grid mt-2">
-<a href="tweet.html"
-class="btn btn-primary btn-block rounded-pill">
+<a href="#" class="btn btn-primary btn-block">
 Tweet</a > </div>
 <script type="text/javascript">
 
 $(function () {
-    var listItems = $(".list-group-item");
-
-    // Remove 'active' tag for all list items
-    for (let i = 0; i < listItems.length; i++) {
-        listItems[i].classList.remove("active");
+    var lI = $(".list-group-item");
+    for (let j = 0; j < lI.length; j++) {lI[j].classList.remove("active");
     }
-
-    var url = window.location.href;
-    var webpage = url.split("/");
-    var web = webpage[webpage.length-1].split('.');
-    var w = web[0];
+    var html = window.location.href;
+    var webpage = html.split("/");
+    var w = webpage[webpage.length-1].split('.')[0];
     console.log(w);
     document.getElementById(w).classList.add("active");
 
-    $('.list-group-item').click(function (e) {
-       $('.list-group-item').removeClass("active");
-        var t = $(e.target);
-        t.addClass("active");
+    $('.list-group-item').click(function (x) {$('.list-group-item').removeClass("active");
+        var tar = $(x.target);
+        tar.addClass("active");
     })
-    
-    // $('#notifications').click(function (e) {
-    //    $('.list-group-item').removeClass("active");
-    //    $('#notifications').addClass("active");
-    // });
-    // $('#message').click(function () {
-    //    $('.list-group-item').removeClass("active");
-    //    $('#message').addClass("active");
-    // });
-    // $('#bookmarks').click(function () {
-    //    $('.list-group-item').removeClass("active");
-    //    $('#bookmarks').addClass("active");
-    // });
-    // $('#lists').click(function () {
-    //    $('.list-group-item').removeClass("active");
-    //    $('#lists').addClass("active");
-    // });
-    // $('#more').click(function () {
-    //    $('.list-group-item').removeClass("active");
-    //    $('#more').addClass("active");
-    // });
-                        
+                       
 })
 </script>
 `);
