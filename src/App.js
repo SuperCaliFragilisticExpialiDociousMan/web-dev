9ca51357bb28ba8f16e3a5235534dfaf4b8841c7
SuperCaliFragilisticExpialiDociousMan <91343063+SuperCaliFragilisticExpialiDociousMan@users.noreply.github.com>
2022-03-21 18:36:53 -0400

    a7_new

--- src/App.js
+++ src/App.js
@@ -12,34 +12,33 @@ import Tuiter from "./components/Tuiter";
 import ExploreScreen from "./components/Tuiter/ExploreScreen/ExploreScreen";
 import HomeScreen from "./components/Tuiter/HomeScreen/HomeScreen";
 import {BrowserRouter, Route} from "react-router-dom";
-
+import { BrowserRouter as Routes} from "react-router-dom";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <div className="container">
-              <Route path="/hello" exact={true}>
-                  <HelloWorld/>
-              </Route>
-              <Route path={["/", "/labs"]} exact={true}>
-                  <Labs/>
-              </Route>
-              <Route path="/tuiter" exact={true}>
-                  <Tuiter/>
-              </Route>
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/">
+                        <Route path="labs"
+                               element={<Labs/>}/>
+                        <Route path="hello"
+                               element={<HelloWorld/>}/>
+                        <Route path="tuiter"
+                               element={<Tuiter/>}>
+                            <Route index
+                                   element={<HomeScreen/>}/>
+                            <Route path="explore"
+                                   element={<ExploreScreen/>}/>
 
-              <Route path="/tuiter/explore" component={ExploreScreen}/>
-              <Route path="/tuiter/home" component={HomeScreen}/>
-
-
-
-          </div>
-      </BrowserRouter>
-
-
-
-  );
+                        </Route>
+                    </Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
+
 export default App;
 
 
